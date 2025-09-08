ddebf104fd8c564f3f288a92ed60dbd84f5e61ef
hoanganhvannham-eng <hoanganhvannham@gmail.com>
2025-09-08 11:51:12 +0700

    update cham cong degin

--- ChamCong.Designer.cs
+++ ChamCong.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1150, 538);
             this.Name = "ChamCong";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ChamCong";
