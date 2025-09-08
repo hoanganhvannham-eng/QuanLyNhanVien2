ac4a9929ff1507ab82d6b501bb37df9a01cc7ce4
hoanganhvannham-eng <hoanganhvannham@gmail.com>
2025-09-08 23:14:36 +0700

    tra lai nhu cu

--- ChamCong.cs
+++ ChamCong.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-// hoan tuan anh
 namespace QuanLyNhanVien2
 {
     public partial class ChamCong: Form
